30d34ecf2a23d74093f4ec4f6d7246a73e7284da
Mohamed Abd Elhakime <you@example.com>
2024-03-17 16:47:13 +0200

    add shared preferences to app

---
+++ lib/core/helper/SharedPreferences/pref.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Pref {
+  //save
+  static saveStringToPref({required String key, required String value}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+  }
+
+  static saveIntToPref({required String key, required int value}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt(key, value);
+  }
+
+  static saveBoolToPref({required String key, required bool value}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(key, value);
+  }
+
+//Read
+  static Future<String> getStringFromPref({required String key}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String stringValue = prefs.getString(key) ?? "";
+    return stringValue;
+  }
+
+  static Future<int> getIntFromPref({required String key}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int intValue = prefs.getInt(key) ?? -1;
+    return intValue;
+  }
+
+  static Future<bool> getBoolFromPref({required String key}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool boolValue = prefs.getBool(key) ?? false;
+    return boolValue;
+  }
+}
