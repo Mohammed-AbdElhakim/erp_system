048d5a89e0c8095bd06a9535c931b5fc9bf9e161
Mohamed Abd Elhakime <you@example.com>
2024-03-31 11:01:42 +0200

    implement home remote data sources.

--- lib/features/home/data/data_sources/home_remote_data_sources.dart
+++ lib/features/home/data/data_sources/home_remote_data_sources.dart
@@ -1,3 +1,6 @@
+import '../../../../core/helper/SharedPreferences/pref.dart';
+import '../../../../core/utils/api_service.dart';
+import '../../../../core/utils/app_strings.dart';
 import '../models/menu_model.dart';
 
 abstract class HomeRemoteDataSources {
@@ -5,9 +8,25 @@ abstract class HomeRemoteDataSources {
 }
 
 class HomeRemoteDataSourcesImpl extends HomeRemoteDataSources {
+  final ApiService apiService;
+
+  HomeRemoteDataSourcesImpl(this.apiService);
+
   @override
-  Future<MenuModel> getMenu() {
-    // TODO: implement getMenu
-    throw UnimplementedError();
+  Future<MenuModel> getMenu() async {
+    String companyKey =
+        await Pref.getStringFromPref(key: AppStrings.companyIdentifierKey) ??
+            "";
+    String token = await Pref.getStringFromPref(key: AppStrings.tokenKey) ?? "";
+    Map<String, dynamic> data = await apiService.get(
+      endPoint: "home/getmenu",
+      headers: {
+        "Authorization": "Bearer $token",
+        "CompanyKey": companyKey,
+      },
+    );
+    MenuModel menuModel = MenuModel.fromJson(data);
+
+    return menuModel;
   }
 }
