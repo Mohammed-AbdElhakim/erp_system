a81102d692c15a4473a418fdbc6584e25b572caf
Mohamed Abd Elhakime <you@example.com>
2024-04-01 15:01:55 +0200

    integrate get Screen cubit.

--- lib/features/screensView/data/repositories/screen_repo_impl.dart
+++ lib/features/screensView/data/repositories/screen_repo_impl.dart
@@ -21,13 +21,130 @@ class ScreenRepoImpl implements ScreenRepo {
               "";
       String token =
           await Pref.getStringFromPref(key: AppStrings.tokenKey) ?? "";
-      Map<String, dynamic> data = await apiService.get(
-        endPoint: "home/getmenu",
-        headers: {
-          "Authorization": "Bearer $token",
-          "CompanyKey": companyKey,
-        },
-      );
+      // Map<String, dynamic> data = await apiService.get(
+      //   endPoint: "home/getmenu",
+      //   headers: {
+      //     "Authorization": "Bearer $token",
+      //     "CompanyKey": companyKey,
+      //   },
+      // );
+      Map<String, dynamic> data = {
+        "columList": [
+          {
+            "id": 131749,
+            "ListName": "offerstates",
+            "ColumnName": "EnName",
+            "enColumnLabel": " English Name ",
+            "arColumnLabel": "الاسم بالانجليزية",
+            "InsertVisable": true,
+            "InsertType": "text",
+            "userID": 4121,
+            "ListID": 16843,
+            "sort": 1,
+            "visible": true,
+            "Cvisable": true,
+            "validation": "required",
+            "isKey": false,
+            "IsRquired": false,
+            "SearchName": "EnName",
+            "IsTotal": false,
+            "InsertDefult": true,
+            "VisableDefult": true,
+            "Width": 140,
+            "MobileVisable": true,
+            "ComID": 1,
+            "MasterInsertVisible": true
+          },
+          {
+            "id": 131748,
+            "ListName": "offerstates",
+            "ColumnName": "ArName",
+            "enColumnLabel": " Arabic Name ",
+            "arColumnLabel": "الاسم بالعربية",
+            "InsertVisable": true,
+            "InsertType": "text",
+            "userID": 4121,
+            "ListID": 16842,
+            "sort": 2,
+            "visible": true,
+            "Cvisable": true,
+            "validation": "required",
+            "isKey": false,
+            "IsRquired": false,
+            "SearchName": "ArName",
+            "IsTotal": false,
+            "InsertDefult": true,
+            "VisableDefult": true,
+            "Width": 140,
+            "MobileVisable": true,
+            "ComID": 1,
+            "MasterInsertVisible": true
+          },
+          {
+            "id": 131750,
+            "ListName": "offerstates",
+            "ColumnName": "Id",
+            "enColumnLabel": " English Name ",
+            "arColumnLabel": "ID",
+            "InsertVisable": false,
+            "InsertType": "number",
+            "userID": 4121,
+            "ListID": 16844,
+            "sort": 3,
+            "visible": false,
+            "Cvisable": false,
+            "validation": "required",
+            "isKey": false,
+            "IsRquired": false,
+            "SearchName": "Id",
+            "IsTotal": false,
+            "InsertDefult": true,
+            "VisableDefult": true,
+            "Width": 140,
+            "MobileVisable": true,
+            "ComID": 1,
+            "MasterInsertVisible": false
+          },
+          {
+            "id": 131757,
+            "ListName": "offerstates",
+            "ColumnName": "Color",
+            "enColumnLabel": "Color",
+            "arColumnLabel": "اللون",
+            "InsertVisable": true,
+            "InsertType": "color",
+            "userID": 4121,
+            "ListID": 16863,
+            "sort": 4,
+            "visible": true,
+            "Cvisable": true,
+            "validation": "required",
+            "isKey": false,
+            "IsRquired": false,
+            "SearchName": "Color",
+            "IsTotal": false,
+            "InsertDefult": true,
+            "VisableDefult": true,
+            "Width": 140,
+            "MobileVisable": true,
+            "ComID": 1,
+            "MasterInsertVisible": true
+          }
+        ],
+        "dataList": {
+          "dynamicList": [
+            {"Id": 3, "ArName": "Demo", "EnName": "Demo", "Color": "#56e907"},
+            {
+              "Id": 2,
+              "ArName": "waiting",
+              "EnName": "waiting",
+              "Color": "#0bf4ae"
+            },
+            {"Id": 1, "ArName": "open", "EnName": "open", "Color": "#10f4f0"}
+          ],
+          "numberofrecords": 3
+        }
+      };
       ScreenModel screenModel = ScreenModel.fromJson(data);
 
       return right(screenModel);
--- lib/features/screensView/presentation/views/screen_view.dart
+++ lib/features/screensView/presentation/views/screen_view.dart
@@ -1,6 +1,10 @@
 import 'package:erp_system/core/utils/app_strings.dart';
 import 'package:erp_system/core/widgets/custom_app_bar.dart';
+import 'package:erp_system/core/widgets/custom_error_massage.dart';
+import 'package:erp_system/core/widgets/custom_loading_widget.dart';
+import 'package:erp_system/features/screensView/presentation/manager/getScreen/get_screen_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/models/menu_model/pages.dart';
 import '../../../../core/utils/app_styles.dart';
@@ -8,6 +12,7 @@ import '../../../../core/widgets/custom_container.dart';
 
 class ScreenView extends StatefulWidget {
   const ScreenView({super.key, required this.pageData});
+
   final Pages pageData;
 
   @override
@@ -16,6 +21,7 @@ class ScreenView extends StatefulWidget {
 
 class _ScreenViewState extends State<ScreenView> {
   String? lang;
+
   @override
   void didChangeDependencies() {
     lang = Localizations.localeOf(context).toString();
@@ -53,7 +59,17 @@ class _ScreenViewState extends State<ScreenView> {
       case "":
         return;
       default:
-        return const SizedBox();
+        return BlocBuilder<GetScreenCubit, GetScreenState>(
+          builder: (context, state) {
+            if (state is GetScreenSuccess) {
+              return const Text("MOHAMED");
+            } else if (state is GetScreenFailure) {
+              return CustomErrorMassage(errorMassage: state.errorMassage);
+            } else {
+              return const CustomLoadingWidget();
+            }
+          },
+        );
     }
   }
 }
