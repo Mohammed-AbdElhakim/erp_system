dec5f6fcc99c0264278163827b585b6d59236e48
Mohamed Abd Elhakime <you@example.com>
2024-04-02 16:52:12 +0200

    create attendance state.

--- lib/features/attendance/presentation/manager/attendanceCubit/attendance_cubit.dart
+++ lib/features/attendance/presentation/manager/attendanceCubit/attendance_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:erp_system/features/attendance/data/models/location_model.dart';
+
+import '../../../data/models/valid_device_model.dart';
 
 part 'attendance_state.dart';
 
--- lib/features/attendance/presentation/manager/attendanceCubit/attendance_state.dart
+++ lib/features/attendance/presentation/manager/attendanceCubit/attendance_state.dart
@@ -2,9 +2,30 @@ part of 'attendance_cubit.dart';
 
 sealed class AttendanceState extends Equatable {
   const AttendanceState();
-}
-
-final class AttendanceInitial extends AttendanceState {
   @override
   List<Object> get props => [];
 }
+
+final class AttendanceInitial extends AttendanceState {}
+
+final class AttendanceLoading extends AttendanceState {}
+
+final class AttendanceFailure extends AttendanceState {
+  final String errorMassage;
+
+  const AttendanceFailure(this.errorMassage);
+}
+
+final class ValidDeviceSuccess extends AttendanceState {
+  final ValidDeviceModel validDeviceModel;
+
+  const ValidDeviceSuccess(this.validDeviceModel);
+}
+
+final class GetAttendanceLocationsSuccess extends AttendanceState {
+  final LocationModel locationModel;
+
+  const GetAttendanceLocationsSuccess(this.locationModel);
+}
+
+final class SendAttendanceSuccess extends AttendanceState {}
