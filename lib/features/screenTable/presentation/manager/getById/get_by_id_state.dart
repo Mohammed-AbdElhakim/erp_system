9caf0631e8001df88e747d8e71f9d2d985a1a8e3
Mohamed Abd Elhakime <you@example.com>
2024-04-18 13:51:25 +0200

    implement GetById State.

--- lib/features/screenTable/presentation/manager/getById/get_by_id_state.dart
+++ lib/features/screenTable/presentation/manager/getById/get_by_id_state.dart
@@ -1,6 +1,24 @@
 part of 'get_by_id_cubit.dart';
 
 @immutable
-sealed class GetByIdState {}
+sealed class GetByIdState extends Equatable {
+  const GetByIdState();
+  @override
+  List<Object> get props => [];
+}
 
 final class GetByIdInitial extends GetByIdState {}
+
+final class GetByIdLoading extends GetByIdState {}
+
+final class GetByIdFailure extends GetByIdState {
+  final String errorMassage;
+
+  const GetByIdFailure(this.errorMassage);
+}
+
+final class GetByIdSuccess extends GetByIdState {
+  final Map<String, dynamic> valueGetById;
+
+  const GetByIdSuccess(this.valueGetById);
+}
