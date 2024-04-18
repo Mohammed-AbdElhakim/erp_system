8122e270e9fc7e852396dddf72ff9f00f2bddeb1
Mohamed Abd Elhakime <you@example.com>
2024-04-18 13:59:15 +0200

    implement GetById cubit.

--- lib/features/screenTable/presentation/manager/getById/get_by_id_cubit.dart
+++ lib/features/screenTable/presentation/manager/getById/get_by_id_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:erp_system/features/screenTable/data/repositories/screen_repo.dart';
 import 'package:meta/meta.dart';
+
+import '../../../../../core/errors/failures.dart';
 
 part 'get_by_id_state.dart';
 
 class GetByIdCubit extends Cubit<GetByIdState> {
-  GetByIdCubit() : super(GetByIdInitial());
+  GetByIdCubit(this.screenRepo) : super(GetByIdInitial());
+  final ScreenRepo screenRepo;
+  Future<void> getById({
+    required String id,
+    required String controllerName,
+  }) async {
+    emit(GetByIdLoading());
+    Either<Failure, Map<String, dynamic>> result =
+        await screenRepo.getById(controllerName, id);
+    result.fold((failure) {
+      emit(GetByIdFailure(failure.errorMassage));
+    }, (valueGetById) {
+      emit(GetByIdSuccess(valueGetById));
+    });
+  }
 }
