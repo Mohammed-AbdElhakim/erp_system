2c4aeae51fbef752eb21de58b43a7ac0abe22420
Mohamed Abd Elhakime <you@example.com>
2024-04-21 23:17:37 +0200

    implement add edit state.

--- lib/features/screenTable/presentation/manager/addEdit/add_edit_state.dart
+++ lib/features/screenTable/presentation/manager/addEdit/add_edit_state.dart
@@ -2,9 +2,22 @@ part of 'add_edit_cubit.dart';
 
 sealed class AddEditState extends Equatable {
   const AddEditState();
-}
-
-final class AddEditInitial extends AddEditState {
   @override
   List<Object> get props => [];
+}
+
+final class AddEditInitial extends AddEditState {}
+
+final class AddEditLoading extends AddEditState {}
+
+final class AddEditFailure extends AddEditState {
+  final String errorMassage;
+
+  const AddEditFailure(this.errorMassage);
+}
+
+final class AddEditSuccess extends AddEditState {
+  // final LoginModel loginModel;
+
+  // const AddEditSuccess(this.loginModel);
 }
