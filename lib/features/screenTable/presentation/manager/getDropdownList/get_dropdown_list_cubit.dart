ad378f32b07fd296e04145e736e9727ace7ff29f
Mohamed Abd Elhakime <you@example.com>
2024-04-22 17:55:41 +0200

    implement get dropdown list cubit.

--- lib/features/screenTable/presentation/manager/getDropdownList/get_dropdown_list_cubit.dart
+++ lib/features/screenTable/presentation/manager/getDropdownList/get_dropdown_list_cubit.dart
@@ -1,9 +1,34 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:erp_system/features/screenTable/data/models/dropdown_model.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/repositories/screen_repo.dart';
 
 part 'get_dropdown_list_state.dart';
 
 class GetDropdownListCubit extends Cubit<GetDropdownListState> {
-  GetDropdownListCubit() : super(GetDropdownListInitial());
+  GetDropdownListCubit(this.screenRepo) : super(GetDropdownListInitial());
+  final ScreenRepo screenRepo;
+  Future<void> getDropdownList({
+    required String droModel,
+    required String droValue,
+    required String droText,
+    required String droCondition,
+    required String droCompany,
+  }) async {
+    emit(GetDropdownListLoading());
+    Either<Failure, DropdownModel> result = await screenRepo.getDropdownList(
+        droModel: droModel,
+        droValue: droValue,
+        droText: droText,
+        droCondition: droCondition,
+        droCompany: droCompany);
+    result.fold((failure) {
+      emit(GetDropdownListFailure(failure.errorMassage));
+    }, (dropdownList) {
+      emit(GetDropdownListSuccess(dropdownList));
+    });
+  }
 }
