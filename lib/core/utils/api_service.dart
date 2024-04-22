adc19917d4c9c37a835c01f809aefc570fdf127f
Mohamed Abd Elhakime <you@example.com>
2024-04-22 13:36:33 +0200

    create put method in api.

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -37,13 +37,29 @@ class ApiService {
     return response.data;
   }
 
-  Future<String> postAttendance({
+  Future<String> post2({
     required String endPoint,
     required Object? data,
     required Map<String, dynamic>? headers,
   }) async {
     String host = await Pref.getStringFromPref(key: AppStrings.hostKey) ?? "";
     Response response = await _dio.post(
+      "$host$_baseUrl$endPoint",
+      data: data,
+      options: Options(
+        headers: headers,
+      ),
+    );
+    return response.data;
+  }
+
+  Future<String> put({
+    required String endPoint,
+    required Object? data,
+    required Map<String, dynamic>? headers,
+  }) async {
+    String host = await Pref.getStringFromPref(key: AppStrings.hostKey) ?? "";
+    Response response = await _dio.put(
       "$host$_baseUrl$endPoint",
       data: data,
       options: Options(
