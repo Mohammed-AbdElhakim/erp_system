52531904ab7488f05dd9abb262774cb5e95ed109
Mohamed Abd Elhakime <you@example.com>
2024-05-02 12:26:01 +0300

    add methods to get data from statement.

--- lib/core/utils/methods.dart
+++ lib/core/utils/methods.dart
@@ -5,3 +5,229 @@ bool isOrientationPortrait(BuildContext context) {
   bool isPortrait = orientation == Orientation.portrait;
   return isPortrait;
 }
+
+String getStringNumber({
+  required String statement,
+  required String search,
+  required String type,
+}) {
+  String s1 = '', s2 = '';
+
+  if (statement.contains("$search >=") &&
+      statement.contains("$search <=") &&
+      statement.contains("$search <>")) {
+    print(1);
+    if (type == "F") {
+      s1 = statement.substring(
+          statement.indexOf("$search >=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    } else if (type == "T") {
+      s1 = statement.substring(
+          statement.indexOf("$search <=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    } else if (type == "N") {
+      s1 = statement.substring(
+          statement.indexOf("$search <>") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+  } else if (statement.contains("$search >=") &&
+      statement.contains("$search <=")) {
+    print("2");
+    if (type == "F") {
+      s1 = statement.substring(
+          statement.indexOf("$search >=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    } else if (type == "T") {
+      s1 = statement.substring(
+          statement.indexOf("$search <=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+  } else if (statement.contains("$search <=") &&
+      statement.contains("$search <>")) {
+    print("3");
+    if (type == "T") {
+      s1 = statement.substring(
+          statement.indexOf("$search <=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    } else if (type == "N") {
+      s1 = statement.substring(
+          statement.indexOf("$search <>") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+  } else if (statement.contains("$search >=") &&
+      statement.contains("$search <>")) {
+    print("4");
+    if (type == "F") {
+      s1 = statement.substring(
+          statement.indexOf("$search >=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    } else if (type == "N") {
+      s1 = statement.substring(
+          statement.indexOf("$search <>") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+  } else if (statement.contains("$search >=")) {
+    print(5);
+    if (type == "F") {
+      s1 = statement.substring(
+          statement.indexOf("$search >=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+  } else if (statement.contains("$search <=")) {
+    print(6);
+    if (type == "T") {
+      s1 = statement.substring(
+          statement.indexOf("$search <=") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+  } else if (statement.contains("$search <>")) {
+    print(7);
+    if (type == "N") {
+      s1 = statement.substring(
+          statement.indexOf("$search <>") + (search.length) + 4,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(' '));
+      return s2;
+    }
+    return s2;
+  }
+
+  return "";
+}
+
+String getStringText({
+  required String statement,
+  required String search,
+}) {
+  String s1 = '', s2 = '';
+  if (statement.contains(search)) {
+    s1 = statement.substring(
+        (statement.indexOf(search) + (search.length) + 9), statement.length);
+
+    s2 = s1.substring(0, s1.indexOf('%'));
+  }
+  return s2;
+}
+
+String getStringDate({
+  required String statement,
+  required String search,
+  required String type,
+}) {
+  String s1 = '', s2 = '';
+
+  if (statement.contains("$search )>= Convert(date, '") &&
+      statement.contains("$search) <= Convert(date, '")) {
+    print(1);
+    if (type == "F") {
+      s1 = statement.substring(
+          statement.indexOf("$search )>= Convert(date, '") +
+              (search.length) +
+              20,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf("'"));
+      return s2;
+    } else if (type == "T") {
+      s1 = statement.substring(
+          statement.indexOf("$search) <= Convert(date, '") +
+              (search.length) +
+              20,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf("'"));
+      return s2;
+    }
+  } else if (statement.contains("$search )>= Convert(date, '")) {
+    print(2);
+    if (type == "F") {
+      s1 = statement.substring(
+          statement.indexOf("$search )>= Convert(date, '") +
+              (search.length) +
+              20,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf("'"));
+      return s2;
+    }
+  } else if (statement.contains("$search) <= Convert(date, '")) {
+    print(3);
+    if (type == "T") {
+      s1 = statement.substring(
+          statement.indexOf("$search) <= Convert(date, '") +
+              (search.length) +
+              20,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf("'"));
+      return s2;
+    }
+  }
+
+  return "";
+}
+
+String getStringDropdown({
+  required String statement,
+  required String search,
+}) {
+  String s1 = '', s2 = '';
+
+  if (statement.contains("$search =")) {
+    if (statement.contains("or $search =")) {
+      print(1);
+      s1 =
+          statement.substring(statement.indexOf("$search ="), statement.length);
+      s2 = s1.substring(0, s1.indexOf(" )"));
+
+      return s2;
+    } else if (statement.contains("$search =")) {
+      print(2);
+      s1 = statement.substring(
+          statement.indexOf("$search =") + (search.length) + 3,
+          statement.length);
+      s2 = s1.substring(0, s1.indexOf(" "));
+      return s2;
+    }
+  }
+
+  return "";
+}
+
+String getStringCheckbox({
+  required String statement,
+  required String search,
+}) {
+  String s1 = '', s2 = '';
+  if (statement.contains("$search =")) {
+    s1 = statement.substring(
+        (statement.indexOf("$search =") + (search.length) + 3),
+        statement.length);
+
+    s2 = s1.substring(0, s1.indexOf(' '));
+    if (s2 == "1") {
+      return "True";
+    } else {
+      return 'False';
+    }
+  }
+  return "";
+}
