5fcb58517e3cee2ab349d87f2e1386a7aa9c6989
Mohamed Abd Elhakime <you@example.com>
2024-05-05 13:28:01 +0300

    implement delete state.

--- lib/features/screenTable/presentation/manager/delete/delete_state.dart
+++ lib/features/screenTable/presentation/manager/delete/delete_state.dart
@@ -2,9 +2,18 @@ part of 'delete_cubit.dart';
 
 sealed class DeleteState extends Equatable {
   const DeleteState();
-}
-
-final class DeleteInitial extends DeleteState {
   @override
   List<Object> get props => [];
 }
+
+final class DeleteInitial extends DeleteState {}
+
+final class DeleteLoading extends DeleteState {}
+
+final class DeleteFailure extends DeleteState {
+  final String errorMassage;
+
+  const DeleteFailure(this.errorMassage);
+}
+
+final class DeleteSuccess extends DeleteState {}
