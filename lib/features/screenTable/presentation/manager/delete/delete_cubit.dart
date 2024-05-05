4ec788a9d0083e257f002ed9086bd5f93105f509
Mohamed Abd Elhakime <you@example.com>
2024-05-05 13:32:08 +0300

    implement delete cubit.

--- lib/features/screenTable/presentation/manager/delete/delete_cubit.dart
+++ lib/features/screenTable/presentation/manager/delete/delete_cubit.dart
@@ -1,8 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/repositories/screen_repo.dart';
 
 part 'delete_state.dart';
 
 class DeleteCubit extends Cubit<DeleteState> {
-  DeleteCubit() : super(DeleteInitial());
+  DeleteCubit(this.screenRepo) : super(DeleteInitial());
+  final ScreenRepo screenRepo;
+
+  Future<void> deleteItems({
+    required String controllerName,
+    required List<String> listId,
+  }) async {
+    emit(DeleteLoading());
+    Either<Failure, String> result = await screenRepo.deleteItem(
+        controllerName: controllerName, listId: listId);
+    result.fold((failure) {
+      emit(DeleteFailure(failure.errorMassage));
+    }, (value) {
+      emit(DeleteSuccess());
+    });
+  }
 }
