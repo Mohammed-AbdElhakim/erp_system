b9d7ad994c911efbcdf7616f7704f90047cfb87d
Mohamed Abd Elhakime <you@example.com>
2024-05-06 11:42:05 +0300

    implement task cubit.

--- lib/features/tasks/presentation/manager/task/task_cubit.dart
+++ lib/features/tasks/presentation/manager/task/task_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:erp_system/features/tasks/data/models/task_model.dart';
+import 'package:erp_system/features/tasks/data/repositories/task_repo.dart';
+
+import '../../../../../core/errors/failures.dart';
 
 part 'task_state.dart';
 
 class TaskCubit extends Cubit<TaskState> {
-  TaskCubit() : super(TaskInitial());
+  TaskCubit(this.taskRepo) : super(TaskInitial());
+  final TaskRepo taskRepo;
+  Future<void> getTasks() async {
+    emit(TaskLoading());
+    Either<Failure, TaskModel> result = await taskRepo.getTask();
+    result.fold((failure) {
+      emit(TaskFailure(failure.errorMassage));
+    }, (tasks) {
+      emit(TaskSuccess(tasks));
+    });
+  }
 }
