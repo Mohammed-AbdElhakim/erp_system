149fb350137a401358d5e1bb8da3cacfdd9f3725
Mohamed Abd Elhakime <you@example.com>
2024-05-06 11:33:11 +0300

    implement get task.

--- lib/features/tasks/data/repositories/task_repo_impl.dart
+++ lib/features/tasks/data/repositories/task_repo_impl.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:erp_system/core/errors/failures.dart';
 import 'package:erp_system/features/tasks/data/models/task_model.dart';
 
+import '../../../../core/helper/SharedPreferences/pref.dart';
 import '../../../../core/utils/api_service.dart';
+import '../../../../core/utils/app_strings.dart';
 import 'task_repo.dart';
 
 class TaskRepoImpl implements TaskRepo {
@@ -11,8 +14,34 @@ class TaskRepoImpl implements TaskRepo {
   TaskRepoImpl(this.apiService);
 
   @override
-  Future<Either<Failure, TaskModel>> getTask() {
-    // TODO: implement getTask
-    throw UnimplementedError();
+  Future<Either<Failure, TaskModel>> getTask() async {
+    try {
+      String companyKey =
+          await Pref.getStringFromPref(key: AppStrings.companyIdentifierKey) ??
+              "";
+      String token =
+          await Pref.getStringFromPref(key: AppStrings.tokenKey) ?? "";
+      Map<String, dynamic> data = await apiService.get(
+        endPoint: "home/getGeneralTable",
+        headers: {
+          "Authorization": "Bearer $token",
+          "CompanyKey": companyKey,
+        },
+      );
+      TaskModel taskModel = TaskModel.fromJson(data);
+
+      return right(taskModel);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
