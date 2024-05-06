47a9f27f2e3de423967ad6acb0e07bfceb749274
Mohamed Abd Elhakime <you@example.com>
2024-05-06 23:57:06 +0300

    implement move tasks state.

--- lib/features/tasks/presentation/manager/move_task/move_task_state.dart
+++ lib/features/tasks/presentation/manager/move_task/move_task_state.dart
@@ -2,9 +2,24 @@ part of 'move_task_cubit.dart';
 
 sealed class MoveTaskState extends Equatable {
   const MoveTaskState();
-}
-
-final class MoveTaskInitial extends MoveTaskState {
   @override
   List<Object> get props => [];
 }
+
+final class MoveTaskInitial extends MoveTaskState {}
+
+final class MoveTaskLoading extends MoveTaskState {}
+
+final class MoveTaskFailure extends MoveTaskState {
+  final String errorMassage;
+
+  const MoveTaskFailure(this.errorMassage);
+}
+
+final class MoveTaskToProgress extends MoveTaskState {}
+
+final class MoveTaskToToDo extends MoveTaskState {}
+
+final class MoveTaskToUnderRevision extends MoveTaskState {}
+
+final class MoveTaskToDone extends MoveTaskState {}
