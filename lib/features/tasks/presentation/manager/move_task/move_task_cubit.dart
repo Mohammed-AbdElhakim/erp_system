c066960b6daf5ed8263007a3e80f749bfcf782b8
Mohamed Abd Elhakime <you@example.com>
2024-05-07 11:35:45 +0300

    implement move tasks cubit.

--- lib/features/tasks/presentation/manager/move_task/move_task_cubit.dart
+++ lib/features/tasks/presentation/manager/move_task/move_task_cubit.dart
@@ -1,8 +1,53 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/repositories/task_repo.dart';
 
 part 'move_task_state.dart';
 
 class MoveTaskCubit extends Cubit<MoveTaskState> {
-  MoveTaskCubit() : super(MoveTaskInitial());
+  MoveTaskCubit(this.taskRepo) : super(MoveTaskInitial());
+  final TaskRepo taskRepo;
+
+  Future<void> moveTaskToToDo(String id) async {
+    emit(MoveTaskLoading());
+    Either<Failure, String> result = await taskRepo.moveTaskToToDo(id);
+    result.fold((failure) {
+      emit(MoveTaskFailure(failure.errorMassage));
+    }, (tasks) {
+      emit(MoveTaskToToDo());
+    });
+  }
+
+  Future<void> moveTaskToProgress(String id) async {
+    emit(MoveTaskLoading());
+    Either<Failure, String> result = await taskRepo.moveTaskToProgress(id);
+    result.fold((failure) {
+      emit(MoveTaskFailure(failure.errorMassage));
+    }, (tasks) {
+      emit(MoveTaskToProgress());
+    });
+  }
+
+  Future<void> moveTaskToUnderRevision(String id) async {
+    emit(MoveTaskLoading());
+    Either<Failure, String> result = await taskRepo.moveTaskToUnderRevision(id);
+    result.fold((failure) {
+      emit(MoveTaskFailure(failure.errorMassage));
+    }, (tasks) {
+      emit(MoveTaskToUnderRevision());
+    });
+  }
+
+  Future<void> moveTaskToDone(String id) async {
+    emit(MoveTaskLoading());
+    Either<Failure, String> result = await taskRepo.moveTaskToDone(id);
+    result.fold((failure) {
+      emit(MoveTaskFailure(failure.errorMassage));
+    }, (tasks) {
+      emit(MoveTaskToDone());
+    });
+  }
 }
