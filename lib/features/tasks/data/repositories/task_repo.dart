2958f7b12eed952da29b0722ea520721f36af877
Mohamed Abd Elhakime <you@example.com>
2024-05-07 11:36:40 +0300

    add methods to move task.

--- lib/features/tasks/data/repositories/task_repo.dart
+++ lib/features/tasks/data/repositories/task_repo.dart
@@ -6,4 +6,8 @@ import '../models/task_model.dart';
 abstract class TaskRepo {
   Future<Either<Failure, List<TaskModel>>> getTask();
   Future<Either<Failure, TaskModel>> getTaskById(String id);
+  Future<Either<Failure, String>> moveTaskToToDo(String id);
+  Future<Either<Failure, String>> moveTaskToProgress(String id);
+  Future<Either<Failure, String>> moveTaskToUnderRevision(String id);
+  Future<Either<Failure, String>> moveTaskToDone(String id);
 }
