a39563558b431862f691cbc25176d3ced83a2e73
Mohamed Abd Elhakime <you@example.com>
2024-06-05 14:20:28 +0300

    implement getPageDetails cubit.

--- lib/features/screenTable/presentation/manager/getPageDetails/get_page_details_cubit.dart
+++ lib/features/screenTable/presentation/manager/getPageDetails/get_page_details_cubit.dart
@@ -1,10 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../../../core/errors/failures.dart';
 import '../../../data/models/tap_model.dart';
+import '../../../data/repositories/screen_repo.dart';
 
 part 'get_page_details_state.dart';
 
 class GetPageDetailsCubit extends Cubit<GetPageDetailsState> {
-  GetPageDetailsCubit() : super(GetPageDetailsInitial());
+  GetPageDetailsCubit(this.screenRepo) : super(GetPageDetailsInitial());
+  final ScreenRepo screenRepo;
+  Future<void> getPageDetails(int pageId) async {
+    emit(GetPageDetailsLoading());
+    Either<Failure, TapModel> result =
+        await screenRepo.getPageDetails(pageID: pageId);
+    result.fold((failure) {
+      emit(GetPageDetailsFailure(failure.errorMassage));
+    }, (tapDetails) {
+      emit(GetPageDetailsSuccess(tapDetails));
+    });
+  }
 }
