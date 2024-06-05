295e93c946d395f7c6c256a701a346ed6edbd584
Mohamed Abd Elhakime <you@example.com>
2024-06-05 14:17:15 +0300

    implement getPageDetails state.

--- lib/features/screenTable/presentation/manager/getPageDetails/get_page_details_cubit.dart
+++ lib/features/screenTable/presentation/manager/getPageDetails/get_page_details_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../data/models/tap_model.dart';
+
 part 'get_page_details_state.dart';
 
 class GetPageDetailsCubit extends Cubit<GetPageDetailsState> {
--- lib/features/screenTable/presentation/manager/getPageDetails/get_page_details_state.dart
+++ lib/features/screenTable/presentation/manager/getPageDetails/get_page_details_state.dart
@@ -2,9 +2,22 @@ part of 'get_page_details_cubit.dart';
 
 sealed class GetPageDetailsState extends Equatable {
   const GetPageDetailsState();
-}
-
-final class GetPageDetailsInitial extends GetPageDetailsState {
   @override
   List<Object> get props => [];
+}
+
+final class GetPageDetailsInitial extends GetPageDetailsState {}
+
+final class GetPageDetailsLoading extends GetPageDetailsState {}
+
+final class GetPageDetailsFailure extends GetPageDetailsState {
+  final String errorMassage;
+
+  const GetPageDetailsFailure(this.errorMassage);
+}
+
+final class GetPageDetailsSuccess extends GetPageDetailsState {
+  final TapModel tapModel;
+
+  const GetPageDetailsSuccess(this.tapModel);
 }
