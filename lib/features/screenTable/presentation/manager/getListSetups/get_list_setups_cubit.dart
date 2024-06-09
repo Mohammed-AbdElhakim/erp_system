a750ec8f93474b9e2a9cdd7d35ea037cd0ed9e1c
Mohamed Abd Elhakime <you@example.com>
2024-06-09 14:15:40 +0300

    implement get List Setups State.

--- lib/features/screenTable/presentation/manager/getListSetups/get_list_setups_cubit.dart
+++ lib/features/screenTable/presentation/manager/getListSetups/get_list_setups_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:erp_system/features/screenTable/data/models/item_list_setup_model.dart';
 
 part 'get_list_setups_state.dart';
 
--- lib/features/screenTable/presentation/manager/getListSetups/get_list_setups_state.dart
+++ lib/features/screenTable/presentation/manager/getListSetups/get_list_setups_state.dart
@@ -2,9 +2,22 @@ part of 'get_list_setups_cubit.dart';
 
 sealed class GetListSetupsState extends Equatable {
   const GetListSetupsState();
-}
-
-final class GetListSetupsInitial extends GetListSetupsState {
   @override
   List<Object> get props => [];
+}
+
+final class GetListSetupsInitial extends GetListSetupsState {}
+
+final class GetListSetupsLoading extends GetListSetupsState {}
+
+final class GetListSetupsFailure extends GetListSetupsState {
+  final String errorMassage;
+
+  const GetListSetupsFailure(this.errorMassage);
+}
+
+final class GetListSetupsSuccess extends GetListSetupsState {
+  final List<ItemListSetupModel> listSetupModel;
+
+  const GetListSetupsSuccess(this.listSetupModel);
 }
