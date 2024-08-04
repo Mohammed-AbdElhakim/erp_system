c85e37796b9e123ec674adc692166c6134dea543
Mohamed Abd Elhakime <you@example.com>
2024-08-04 16:07:22 +0300

    implement TrialBalanceCubit.

--- lib/features/trialBalance/presentation/manager/trialBalance/trial_balance_cubit.dart
+++ lib/features/trialBalance/presentation/manager/trialBalance/trial_balance_cubit.dart
@@ -1,9 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:erp_system/features/trialBalance/data/models/trial_balance_body_model.dart';
 import 'package:erp_system/features/trialBalance/data/models/trial_balance_model.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/repositories/trial_balance_repo.dart';
 
 part 'trial_balance_state.dart';
 
 class TrialBalanceCubit extends Cubit<TrialBalanceState> {
-  TrialBalanceCubit() : super(TrialBalanceInitial());
+  TrialBalanceCubit(this.trialBalanceRepo) : super(TrialBalanceInitial());
+  final TrialBalanceRepo trialBalanceRepo;
+
+  Future<void> getTrialBalance({
+    required TrialBalanceBodyModel trialBalanceBody,
+  }) async {
+    emit(TrialBalanceLoading());
+    Either<Failure, TrialBalanceModel> result = await trialBalanceRepo
+        .getTrialBalance(trialBalanceBody: trialBalanceBody);
+    result.fold((failure) {
+      emit(TrialBalanceFailure(failure.errorMassage));
+    }, (trialBalance) {
+      emit(TrialBalanceSuccess(trialBalance));
+    });
+  }
 }
