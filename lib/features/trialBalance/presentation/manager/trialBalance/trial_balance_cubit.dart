5560161b5cc82d8f9acc807f00a5a5ac86925c3f
Mohamed Abd Elhakime <you@example.com>
2024-08-04 16:02:12 +0300

    implement TrialBalanceState.

--- lib/features/trialBalance/presentation/manager/trialBalance/trial_balance_cubit.dart
+++ lib/features/trialBalance/presentation/manager/trialBalance/trial_balance_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:erp_system/features/trialBalance/data/models/trial_balance_model.dart';
 
 part 'trial_balance_state.dart';
 
--- lib/features/trialBalance/presentation/manager/trialBalance/trial_balance_state.dart
+++ lib/features/trialBalance/presentation/manager/trialBalance/trial_balance_state.dart
@@ -2,9 +2,22 @@ part of 'trial_balance_cubit.dart';
 
 sealed class TrialBalanceState extends Equatable {
   const TrialBalanceState();
-}
-
-final class TrialBalanceInitial extends TrialBalanceState {
   @override
   List<Object> get props => [];
+}
+
+final class TrialBalanceInitial extends TrialBalanceState {}
+
+final class TrialBalanceLoading extends TrialBalanceState {}
+
+final class TrialBalanceFailure extends TrialBalanceState {
+  final String errorMassage;
+
+  const TrialBalanceFailure(this.errorMassage);
+}
+
+final class TrialBalanceSuccess extends TrialBalanceState {
+  final TrialBalanceModel trialBalanceModel;
+
+  const TrialBalanceSuccess(this.trialBalanceModel);
 }
