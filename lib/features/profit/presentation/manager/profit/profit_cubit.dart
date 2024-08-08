35d48566e3a71c747a5cce066183661cb2b60e43
Mohamed Abd Elhakime <you@example.com>
2024-08-08 10:55:06 +0300

    implement Profit State.

--- lib/features/profit/presentation/manager/profit/profit_cubit.dart
+++ lib/features/profit/presentation/manager/profit/profit_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../data/models/profit_model.dart';
+
 part 'profit_state.dart';
 
 class ProfitCubit extends Cubit<ProfitState> {
--- lib/features/profit/presentation/manager/profit/profit_state.dart
+++ lib/features/profit/presentation/manager/profit/profit_state.dart
@@ -2,9 +2,22 @@ part of 'profit_cubit.dart';
 
 sealed class ProfitState extends Equatable {
   const ProfitState();
-}
-
-final class ProfitInitial extends ProfitState {
   @override
   List<Object> get props => [];
+}
+
+final class ProfitInitial extends ProfitState {}
+
+final class ProfitLoading extends ProfitState {}
+
+final class ProfitFailure extends ProfitState {
+  final String errorMassage;
+
+  const ProfitFailure(this.errorMassage);
+}
+
+final class ProfitSuccess extends ProfitState {
+  final List<ProfitModel> profitModel;
+
+  const ProfitSuccess(this.profitModel);
 }
