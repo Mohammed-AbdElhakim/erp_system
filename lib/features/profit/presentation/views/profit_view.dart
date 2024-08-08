b5ebfeb016a87516ba5601cf1c5c551dc5192f3a
Mohamed Abd Elhakime <you@example.com>
2024-08-08 12:13:38 +0300

    add BlocProvider to ProfitCubit in ProfitView.

--- lib/features/profit/presentation/views/profit_view.dart
+++ lib/features/profit/presentation/views/profit_view.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/models/menu_model/pages.dart';
 import '../../../../core/utils/app_strings.dart';
 import '../../../../core/utils/app_styles.dart';
 import '../../../../core/utils/methods.dart';
+import '../../../../core/utils/service_locator.dart';
 import '../../../../core/widgets/change_status_bar_color.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
 import '../../../../core/widgets/custom_container.dart';
+import '../../data/repositories/profit_repo_impl.dart';
+import '../manager/profit/profit_cubit.dart';
 import '../widgets/profit_view_body.dart';
 
 class ProfitView extends StatefulWidget {
   const ProfitView({super.key, required this.pageData});
+
   final Pages pageData;
+
   @override
   State<ProfitView> createState() => _ProfitViewState();
 }
@@ -27,37 +33,40 @@ class _ProfitViewState extends State<ProfitView> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeStatusBarColor(
-      child: Scaffold(
-        appBar: CustomAppBar(
-          isPortrait: isOrientationPortrait(context),
-          title: lang == AppStrings.enLangKey
-              ? widget.pageData.nameEn
-              : widget.pageData.nameAr,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              isOrientationPortrait(context)
-                  ? CustomContainer(
-                      height: 120,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 12),
-                        child: Text(
-                          lang == AppStrings.enLangKey
-                              ? widget.pageData.nameEn
-                              : widget.pageData.nameAr,
-                          maxLines: 1,
-                          textAlign: TextAlign.center,
-                          style: AppStyles.textStyle26,
+    return BlocProvider(
+      create: (context) => ProfitCubit(getIt.get<ProfitRepoImpl>()),
+      child: ChangeStatusBarColor(
+        child: Scaffold(
+          appBar: CustomAppBar(
+            isPortrait: isOrientationPortrait(context),
+            title: lang == AppStrings.enLangKey
+                ? widget.pageData.nameEn
+                : widget.pageData.nameAr,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                isOrientationPortrait(context)
+                    ? CustomContainer(
+                        height: 120,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 12),
+                          child: Text(
+                            lang == AppStrings.enLangKey
+                                ? widget.pageData.nameEn
+                                : widget.pageData.nameAr,
+                            maxLines: 1,
+                            textAlign: TextAlign.center,
+                            style: AppStyles.textStyle26,
+                          ),
                         ),
+                      )
+                    : const SizedBox(
+                        height: 25,
                       ),
-                    )
-                  : const SizedBox(
-                      height: 25,
-                    ),
-              const ProfitViewBody(),
-            ],
+                const ProfitViewBody(),
+              ],
+            ),
           ),
         ),
       ),
