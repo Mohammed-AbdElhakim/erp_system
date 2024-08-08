ff229ec74df4e0566f0ee39dcda36c65fcde2c4f
Mohamed Abd Elhakime <you@example.com>
2024-08-08 12:06:48 +0300

    add ProfitRepoImpl to setupServiceLocator.

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -5,6 +5,7 @@ import 'package:get_it/get_it.dart';
 import '../../features/attendance/data/repositories/attendance_repo_impl.dart';
 import '../../features/auth/data/repositories/login/login_repo_impl.dart';
 import '../../features/chart/data/repositories/chart_repo_impl.dart';
+import '../../features/profit/data/repositories/trial_balance_repo_impl.dart';
 import '../../features/screenTable/data/repositories/screen_repo_impl.dart';
 import '../../features/tasks/data/repositories/task_repo_impl.dart';
 import '../repositories/menu/menu_repo_impl.dart';
@@ -35,6 +36,12 @@ void setupServiceLocator() {
 
   getIt.registerSingleton<TrialBalanceRepoImpl>(
     TrialBalanceRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+
+  getIt.registerSingleton<ProfitRepoImpl>(
+    ProfitRepoImpl(
       getIt.get<ApiService>(),
     ),
   );
