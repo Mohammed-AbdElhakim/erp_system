cbb025bd1da97b3acdfc77fa07708bfb45fc1c38
Mohamed Abd Elhakime <you@example.com>
2024-08-08 12:10:57 +0300

    implement Profit Cubit.

--- lib/features/profit/presentation/manager/profit/profit_cubit.dart
+++ lib/features/profit/presentation/manager/profit/profit_cubit.dart
@@ -1,10 +1,28 @@
-import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/errors/failures.dart';
+import '../../../data/models/profit_body_model.dart';
 import '../../../data/models/profit_model.dart';
+import '../../../data/repositories/profit_repo.dart';
 
 part 'profit_state.dart';
 
 class ProfitCubit extends Cubit<ProfitState> {
-  ProfitCubit() : super(ProfitInitial());
+  ProfitCubit(this.profitRepo) : super(ProfitInitial());
+  final ProfitRepo profitRepo;
+
+  Future<void> getProfit({
+    required ProfitBodyModel profitBodyModel,
+  }) async {
+    emit(ProfitLoading());
+    Either<Failure, List<ProfitModel>> result =
+        await profitRepo.getProfit(profitBodyModel: profitBodyModel);
+    result.fold((failure) {
+      emit(ProfitFailure(failure.errorMassage));
+    }, (profit) {
+      emit(ProfitSuccess(profit));
+    });
+  }
 }
