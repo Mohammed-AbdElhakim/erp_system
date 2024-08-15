fe5b8e07a5ee3ecee0b3e2b8421169bb7dc6c872
Mohamed Abd Elhakime <you@example.com>
2024-08-15 16:30:03 +0300

    show CashierData.

--- lib/features/cashier/presentation/widgets/cashier_view_body.dart
+++ lib/features/cashier/presentation/widgets/cashier_view_body.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import '../../data/models/modality_model.dart';
 import '../../data/models/pro_company_model.dart';
 import 'custom_app_bar_body.dart';
+import 'custom_drawer.dart';
 
 class CashierViewBody extends StatefulWidget {
   const CashierViewBody(
@@ -22,18 +23,43 @@ class CashierViewBody extends StatefulWidget {
 }
 
 class _CashierViewBodyState extends State<CashierViewBody> {
+  int modalityIdChoose = -1;
+  List<ProductItem> myProductList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBarBody(title: widget.title),
-      drawer: Drawer(
-        child: Container(
-          child: Text(widget.productList[1].proName!),
-        ),
+      drawer: CustomDrawer(
+        proCompanyList: widget.proCompanyList,
+        modalityList: widget.modalityList,
+        onTap: (modalityId) {
+          modalityIdChoose = modalityId;
+          myProductList.clear();
+          for (var i in widget.productList) {
+            if (i.modality == modalityId) {
+              myProductList.add(i);
+            }
+          }
+          setState(() {});
+        },
       ),
-      body: Container(
-        child: Text(widget.productList[1].proName!),
-      ),
+      body: modalityIdChoose == -1
+          ? const Center(
+              child: Text("برجاء اختيار القسم"),
+            )
+          : myProductList.isEmpty
+              ? const Center(
+                  child: Text("لا يوجد منتاجات"),
+                )
+              : ListView.separated(
+                  itemBuilder: (context, index) {
+                    return Text(myProductList[index].proName!);
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: 15);
+                  },
+                  itemCount: myProductList.length,
+                ),
     );
   }
 }
---
+++ lib/features/cashier/presentation/widgets/custom_drawer.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../data/models/modality_model.dart';
+import '../../data/models/pro_company_model.dart';
+import 'custom_main_list_tile_in_drawer_cashier.dart';
+import 'custom_sub_list_tile_in_drawer_cashier.dart';
+
+class CustomDrawer extends StatefulWidget {
+  const CustomDrawer(
+      {super.key,
+      required this.proCompanyList,
+      required this.modalityList,
+      required this.onTap});
+  final List<PRoCompanyItem> proCompanyList;
+  final List<ModalityItem> modalityList;
+  final void Function(int modalityId) onTap;
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView.builder(
+        itemCount: widget.proCompanyList.length,
+        shrinkWrap: true,
+        // physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          PRoCompanyItem proCompanyItem = widget.proCompanyList[index];
+          List<ModalityItem> modalityList = [];
+          for (var i in widget.modalityList) {
+            if (i.companyID == proCompanyItem.companyID) {
+              modalityList.add(i);
+            }
+          }
+          return CustomMainListTileInDrawerCashier(
+            title: proCompanyItem.companyName!,
+            bodyList: List.generate(
+              modalityList.length,
+              (modalityIndex) {
+                return CustomSubListTileInDrawerCashier(
+                  modalityItem: modalityList[modalityIndex],
+                  onTap: (modalityId) {
+                    widget.onTap(modalityId);
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
