cb595d6b13a47145c66c21d68a8453c7be63794f
Mohamed Abd Elhakime <you@example.com>
2024-08-19 14:06:37 +0300

    create pageView and  Get List Setups.

--- lib/features/cashier/presentation/views/cashier_view.dart
+++ lib/features/cashier/presentation/views/cashier_view.dart
@@ -9,6 +9,9 @@ import '../../../../core/widgets/change_status_bar_color.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
 import '../../../../core/widgets/custom_error_massage.dart';
 import '../../../../core/widgets/custom_loading_widget.dart';
+import '../../../screenTable/data/models/item_list_setup_model.dart';
+import '../../../screenTable/data/repositories/screen_repo_impl.dart';
+import '../../../screenTable/presentation/manager/getListSetups/get_list_setups_cubit.dart';
 import '../../data/repositories/cashier_repo_impl.dart';
 import '../manager/cashier/cashier_cubit.dart';
 import '../widgets/cashier_view_body.dart';
@@ -24,6 +27,7 @@ class CashierView extends StatefulWidget {
 
 class _CashierViewState extends State<CashierView> {
   String? lang;
+  int _currentIndex = 0;
 
   @override
   void didChangeDependencies() {
@@ -33,10 +37,18 @@ class _CashierViewState extends State<CashierView> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CashierCubit(
-        getIt.get<CashierRepoImpl>(),
-      )..getCashierData(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CashierCubit(
+            getIt.get<CashierRepoImpl>(),
+          )..getCashierData(),
+        ),
+        BlocProvider(
+          create: (context) => GetListSetupsCubit(getIt.get<ScreenRepoImpl>())
+            ..getListSetups("SalesDetailView"),
+        ),
+      ],
       child: ChangeStatusBarColor(
         child: Scaffold(
           appBar: CustomAppBar(
@@ -44,20 +56,78 @@ class _CashierViewState extends State<CashierView> {
             title: lang == AppStrings.enLangKey
                 ? widget.pageData.nameEn
                 : widget.pageData.nameAr,
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(3, (index) {
+                  return AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    width: _currentIndex == index ? 12 : 8,
+                    height: _currentIndex == index ? 12 : 8,
+                    decoration: BoxDecoration(
+                      color: _currentIndex == index ? Colors.blue : Colors.grey,
+                      shape: BoxShape.circle,
+                    ),
+                  );
+                }),
+              )
+            ],
           ),
           body: BlocBuilder<CashierCubit, CashierState>(
-            builder: (context, state) {
-              if (state is CashierSuccess) {
-                return CashierViewBody(
-                  title: lang == AppStrings.enLangKey
-                      ? widget.pageData.nameEn
-                      : widget.pageData.nameAr,
-                  allModalityList: state.modalityList,
-                  allProCompanyList: state.pRoCompanyList,
-                  allProductList: state.productList,
+            builder: (context, cstate) {
+              if (cstate is CashierSuccess) {
+                return BlocBuilder<GetListSetupsCubit, GetListSetupsState>(
+                  builder: (context, state) {
+                    if (state is GetListSetupsSuccess) {
+                      List<ItemListSetupModel> listSetup = state.listSetupModel;
+                      List<ItemListSetupModel> listColumn = [];
+                      List<dynamic> listKey = [];
+                      List<String> category = [];
+                      List<String> listHeader = [];
+
+                      for (var item in state.listSetupModel) {
+                        category.add(item.categoryTitle ?? '');
+                        if (item.insertVisable == true &&
+                            item.cvisable == true &&
+                            item.visible == true &&
+                            item.isGeneral != true) {
+                          listColumn.add(item);
+                          listKey.add(item.columnName);
+                          listHeader.add(lang == AppStrings.enLangKey
+                              ? item.enColumnLabel!
+                              : item.arColumnLabel!);
+                        }
+                      }
+                      List<String> categoryList = category.toSet().toList();
+                      return CashierViewBody(
+                        title: lang == AppStrings.enLangKey
+                            ? widget.pageData.nameEn
+                            : widget.pageData.nameAr,
+                        allModalityList: cstate.modalityList,
+                        allProCompanyList: cstate.pRoCompanyList,
+                        allProductList: cstate.productList,
+                        listSetup: listSetup,
+                        categoryList: categoryList,
+                        listKey: listKey,
+                        listColumn: listColumn,
+                        listHeader: listHeader,
+                        onPageChanged: (pageIndex) {
+                          setState(() {
+                            _currentIndex = pageIndex;
+                          });
+                        },
+                      );
+                    } else if (state is GetListSetupsFailure) {
+                      return CustomErrorMassage(
+                          errorMassage: state.errorMassage);
+                    } else {
+                      return const CustomLoadingWidget();
+                    }
+                  },
                 );
-              } else if (state is CashierFailure) {
-                return CustomErrorMassage(errorMassage: state.errorMassage);
+              } else if (cstate is CashierFailure) {
+                return CustomErrorMassage(errorMassage: cstate.errorMassage);
               } else {
                 return const CustomLoadingWidget();
               }
--- lib/features/cashier/presentation/widgets/cashier_view_body.dart
+++ lib/features/cashier/presentation/widgets/cashier_view_body.dart
@@ -1,11 +1,10 @@
 import 'package:erp_system/features/cashier/data/models/product_model.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../core/utils/app_colors.dart';
+import '../../../screenTable/data/models/item_list_setup_model.dart';
 import '../../data/models/modality_model.dart';
 import '../../data/models/pro_company_model.dart';
-import 'custom_product_item_widget.dart';
-import 'custom_search.dart';
+import 'page_2.dart';
 
 class CashierViewBody extends StatefulWidget {
   const CashierViewBody(
@@ -13,209 +12,65 @@ class CashierViewBody extends StatefulWidget {
       required this.title,
       required this.allProCompanyList,
       required this.allModalityList,
-      required this.allProductList});
+      required this.allProductList,
+      required this.listSetup,
+      required this.listColumn,
+      required this.listKey,
+      required this.categoryList,
+      required this.listHeader,
+      this.onPageChanged});
   final String title;
   final List<ProCompanyItem> allProCompanyList;
   final List<ModalityItem> allModalityList;
   final List<ProductItem> allProductList;
+  final List<ItemListSetupModel> listSetup;
+  final List<ItemListSetupModel> listColumn;
+  final List<dynamic> listKey;
+  final List<String> categoryList;
+  final List<String> listHeader;
+  final void Function(int)? onPageChanged;
 
   @override
   State<CashierViewBody> createState() => _CashierViewBodyState();
 }
 
 class _CashierViewBodyState extends State<CashierViewBody> {
-  late int companyId;
-  List<ModalityItem> modalityList = [];
-  List<ProductItem> productList = [];
-  late int modalityId;
-  @override
-  void initState() {
-    companyId = widget.allProCompanyList[0].companyID!;
-    for (var i in widget.allModalityList) {
-      if (i.companyID == companyId) {
-        modalityList.add(i);
-      }
-    }
-    modalityId = modalityList[0].modalityID!;
-    for (var i in widget.allProductList) {
-      if (i.modality == modalityId) {
-        productList.add(i);
-      }
-    }
-    super.initState();
-  }
-
+  final PageController _controller = PageController();
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return PageView(
+      controller: _controller,
+      onPageChanged: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+        widget.onPageChanged!(_currentIndex);
+      },
       children: [
-        Expanded(
-            child: Row(
-          children: [
-            Expanded(
-                child: Container(
-              decoration: const BoxDecoration(
-                  border: BorderDirectional(
-                      end: BorderSide(color: Colors.black26))),
-              padding: const EdgeInsets.all(5),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: ListView.separated(
-                  itemCount: modalityList.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        productList.clear();
-                        setState(() {
-                          modalityId = modalityList[index].modalityID!;
-                          for (var i in widget.allProductList) {
-                            if (i.modality == modalityId) {
-                              productList.add(i);
-                            }
-                          }
-                        });
-                      },
-                      child: Container(
-                        width: 100,
-                        height: 35,
-                        padding: const EdgeInsets.all(5),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            color: modalityId == modalityList[index].modalityID
-                                ? AppColors.blueGreyDark
-                                : Colors.white,
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(color: Colors.black)),
-                        child: Text(
-                          modalityList[index].modalityName ?? "",
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: modalityId == modalityList[index].modalityID
-                                ? AppColors.white
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 5),
-                ),
-              ),
-            )),
-            Expanded(
-                flex: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 5, right: 16, left: 16, bottom: 10),
-                      child: InkWell(
-                        onTap: () {
-                          showSearch(
-                            context: context,
-                            delegate: CustomSearch(
-                              productList: widget.allProductList,
-                            ),
-                          );
-                        },
-                        child: CircleAvatar(
-                            backgroundColor: AppColors.blueGreyLight,
-                            child: const Icon(Icons.search)),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Center(
-                          child: SingleChildScrollView(
-                            child: Wrap(
-                              runSpacing: 5.0,
-                              spacing: 5,
-                              children: productList
-                                  .map(
-                                    (e) => CustomProductItemWidget(
-                                      productItem: e,
-                                    ),
-                                  )
-                                  .toList(),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-          ],
-        )),
         Container(
-          height: 86,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              color: Colors.grey.shade300,
-              border: const Border(top: BorderSide(color: Colors.black))),
-          padding: const EdgeInsets.all(5),
+          color: Colors.green,
           child: Center(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Wrap(
-                runSpacing: 5.0,
-                spacing: 5,
-                direction: Axis.vertical,
-                children: widget.allProCompanyList
-                    .map(
-                      (item) => InkWell(
-                        onTap: () {
-                          modalityList.clear();
-                          productList.clear();
-                          setState(() {
-                            companyId = item.companyID!;
-                            for (var i in widget.allModalityList) {
-                              if (i.companyID == companyId) {
-                                modalityList.add(i);
-                              }
-                            }
-                            modalityId = modalityList[0].modalityID!;
-                            for (var i in widget.allProductList) {
-                              if (i.modality == modalityId) {
-                                productList.add(i);
-                              }
-                            }
-                          });
-                        },
-                        child: Container(
-                          width: 100,
-                          height: 35,
-                          padding: const EdgeInsets.all(5),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: companyId == item.companyID
-                                  ? AppColors.blueGreyDark
-                                  : Colors.white,
-                              borderRadius: BorderRadius.circular(8),
-                              border: Border.all(color: Colors.black)),
-                          child: Text(
-                            item.companyName ?? "",
-                            textAlign: TextAlign.center,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: companyId == item.companyID
-                                  ? AppColors.white
-                                  : Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ),
+            child: Text(
+              'Screen 1',
+              style: TextStyle(color: Colors.white, fontSize: 24),
             ),
           ),
-        )
+        ),
+        Page2(
+          allProCompanyList: widget.allProCompanyList,
+          allModalityList: widget.allModalityList,
+          allProductList: widget.allProductList,
+        ),
+        Container(
+          color: Colors.red,
+          child: Center(
+            child: Text(
+              'Screen 3',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+        ),
       ],
     );
   }
