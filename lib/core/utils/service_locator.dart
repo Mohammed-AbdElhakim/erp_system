89d141436ef972cef807ed676d7a63baf3cc1810
Mohamed Abd Elhakime <you@example.com>
2024-09-15 11:54:20 +0300

    refactor service_locator.dart

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:erp_system/features/accountProf/data/repositories/account_prof_repo_impl.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../features/Reports/data/repositories/reports_repo_impl.dart';
+import '../../features/accountProf/data/repositories/account_prof_repo_impl.dart';
 import '../../features/attendance/data/repositories/attendance_repo_impl.dart';
-import '../../features/auth/data/repositories/login/logincashier_repo_impl.dart';
+import '../../features/auth/data/repositories/login/login_repo_impl.dart';
 import '../../features/chart/data/repositories/chart_repo_impl.dart';
 import '../../features/generalBalance/data/repositories/general_balance_repo_impl.dart';
 import '../../features/profit/data/repositories/profit_repo_impl.dart';
