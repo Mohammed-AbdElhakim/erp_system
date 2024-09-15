cf6d743f94ffb0fd55aa75fdb30e98b8ad7bd634
Mohamed Abd Elhakime <you@example.com>
2024-09-15 11:58:04 +0300

    create cubits.

---
+++ lib/features/accountProf/presentation/manager/getTable/get_table_cubit.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/models/screen_model.dart';
+import '../../../data/repositories/account_prof_repo.dart';
+
+part 'get_table_state.dart';
+
+class GetTableCubit extends Cubit<GetTableState> {
+  GetTableCubit(this.accountProfRepo) : super(GetTableInitial());
+  final AccountProfRepo accountProfRepo;
+
+  Future<void> getTable({
+    required int pageId,
+    required bool employee,
+    required bool isdesc,
+    required int limit,
+    required int offset,
+    required String orderby,
+    required String statment,
+    required String selectcolumns,
+    required bool isDepartment,
+    required String departmentName,
+    required int authorizationID,
+    required String viewEmployeeColumn,
+    required int numberOfPage,
+    required int dropdownValueOfLimit,
+  }) async {
+    emit(GetTableLoading());
+    Either<Failure, ScreenModel> result = await accountProfRepo.getTable(
+      pageId: pageId,
+      employee: employee,
+      isdesc: isdesc,
+      limit: limit,
+      offset: offset,
+      orderby: orderby,
+      statment: statment,
+      selectcolumns: selectcolumns,
+      isDepartment: isDepartment,
+      departmentName: departmentName,
+      authorizationID: authorizationID,
+      viewEmployeeColumn: viewEmployeeColumn,
+    );
+    result.fold((failure) {
+      emit(GetTableFailure(failure.errorMassage));
+    }, (menuList) {
+      emit(GetTableSuccess(menuList, numberOfPage, dropdownValueOfLimit));
+    });
+  }
+}
---
+++ lib/features/accountProf/presentation/manager/getTable/get_table_state.dart
@@ -0,0 +1,25 @@
+part of 'get_table_cubit.dart';
+
+sealed class GetTableState extends Equatable {
+  const GetTableState();
+  @override
+  List<Object> get props => [];
+}
+
+final class GetTableInitial extends GetTableState {}
+
+final class GetTableLoading extends GetTableState {}
+
+final class GetTableFailure extends GetTableState {
+  final String errorMassage;
+
+  const GetTableFailure(this.errorMassage);
+}
+
+final class GetTableSuccess extends GetTableState {
+  final ScreenModel screenModel;
+  final int numberPage;
+  final int dropdownValue;
+
+  const GetTableSuccess(this.screenModel, this.numberPage, this.dropdownValue);
+}
