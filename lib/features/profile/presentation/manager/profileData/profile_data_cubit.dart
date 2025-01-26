bbaa7b85815837e9103a24f278d03ef6629e6149
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-01-26 15:07:42 +0200

    create Profile Data cubit

---
+++ lib/features/profile/presentation/manager/profileData/profile_data_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/models/profile_model.dart';
+import '../../../data/repositories/profile_repo.dart';
+
+part 'profile_data_state.dart';
+
+class ProfileDataCubit extends Cubit<ProfileDataState> {
+  ProfileDataCubit(this.profileRepo) : super(ProfileDataInitial());
+
+  final ProfileRepo profileRepo;
+  Future<void> getPagesNotifications() async {
+    emit(ProfileDataLoading());
+    Either<Failure, ProfileModel> result = await profileRepo.getProfileData();
+    result.fold((failure) {
+      emit(ProfileDataFailure(failure.errorMassage));
+    }, (result) {
+      emit(ProfileDataSuccess(result));
+    });
+  }
+}
