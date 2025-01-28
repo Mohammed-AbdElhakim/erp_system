97239b3c173718be898d90811990cf8ba7e375dc
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-01-28 15:09:39 +0200

    add product card repo impl to service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -11,6 +11,7 @@ import '../../features/customerAccount/data/repositories/customer_account_repo_i
 import '../../features/dashboard/data/repositories/dashboard_repo_impl.dart';
 import '../../features/generalBalance/data/repositories/general_balance_repo_impl.dart';
 import '../../features/notifications/data/repositories/notifications_repo_impl.dart';
+import '../../features/productCard/data/repositories/product_card_repo_impl.dart';
 import '../../features/profile/data/repositories/profile_repo_impl.dart';
 import '../../features/profit/data/repositories/profit_repo_impl.dart';
 import '../../features/screenTable/data/repositories/screen_repo_impl.dart';
@@ -121,6 +122,12 @@ void setupServiceLocator() {
 
   getIt.registerSingleton<ProfileRepoImpl>(
     ProfileRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+
+  getIt.registerSingleton<ProductCardRepoImpl>(
+    ProductCardRepoImpl(
       getIt.get<ApiService>(),
     ),
   );
