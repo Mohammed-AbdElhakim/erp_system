2b2ef070ae522d32685d3a9ff3b884103f14dc2e
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-02-03 13:38:07 +0200

    create store show repo.

---
+++ lib/features/storeShow/data/repositories/store_show_repo.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../models/all_dropdown_model.dart';
+import '../models/screen_model.dart';
+import '../models/store_show_model.dart';
+
+abstract class StoreShowRepo {
+  Future<Either<Failure, ScreenModel>> getTable({
+    required int pageId,
+    required bool employee,
+    required bool isdesc,
+    required int limit,
+    required int offset,
+    required String orderby,
+    required String statment,
+    required String selectcolumns,
+    required bool isDepartment,
+    required String departmentName,
+    required int authorizationID,
+    required String viewEmployeeColumn,
+  });
+  Future<Either<Failure, List<AllDropdownModel>>> getAllDropdownList({
+    required int pageID,
+  });
+
+  Future<Either<Failure, String>> add({
+    required String controllerName,
+    required Map<String, dynamic> body,
+  });
+
+  Future<Either<Failure, StoreShowModel>> getTableStoreShow({
+    required Map<String, dynamic> objectData,
+  });
+}
