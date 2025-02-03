587653595d069747caa9f0e9e35534c4f64b7540
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-02-03 13:50:38 +0200

    create add edit cubit.

---
+++ lib/features/storeShow/presentation/manager/addEdit/add_edit_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/repositories/store_show_repo.dart';
+
+part 'add_edit_state.dart';
+
+class AddEditCubit extends Cubit<AddEditState> {
+  AddEditCubit(this.storeShowRepo) : super(AddEditInitial());
+  final StoreShowRepo storeShowRepo;
+  Future<void> add({
+    required String controllerName,
+    required Map<String, dynamic> body,
+  }) async {
+    emit(AddEditLoading());
+    Either<Failure, String> result = await storeShowRepo.add(
+      body: body,
+      controllerName: controllerName,
+    );
+    result.fold((failure) {
+      emit(AddEditFailure(failure.errorMassage));
+    }, (send) {
+      emit(AddEditSuccess());
+    });
+  }
+}
