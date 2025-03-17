6b41d0e13ead78ad5cbaddd7304ee5d14dc9e4fc
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-03-17 16:23:56 +0200

    create daily_french_cash_view.dart and inputs data

--- lib/features/dailyFrenchCash/presentation/views/daily_french_cash_view.dart
+++ lib/features/dailyFrenchCash/presentation/views/daily_french_cash_view.dart
@@ -7,12 +7,16 @@ import '../../../../core/utils/methods.dart';
 import '../../../../core/utils/service_locator.dart';
 import '../../../../core/widgets/change_status_bar_color.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
+import '../../../../core/widgets/custom_error_massage.dart';
+import '../../../../core/widgets/custom_loading_widget.dart';
 import '../../data/repositories/daily_french_cash_repo_impl.dart';
+import '../manager/accounts/accounts_cubit.dart';
 import '../manager/dailyFrenchCash/daily_french_cash_cubit.dart';
 import '../widgets/daily_french_cash_view_body.dart';
 
 class DailyFrenchCashView extends StatefulWidget {
   const DailyFrenchCashView({super.key, required this.pageData});
+
   final Pages pageData;
 
   @override
@@ -21,6 +25,7 @@ class DailyFrenchCashView extends StatefulWidget {
 
 class _DailyFrenchCashViewState extends State<DailyFrenchCashView> {
   String? lang;
+
   @override
   void didChangeDependencies() {
     lang = Localizations.localeOf(context).toString();
@@ -38,9 +43,31 @@ class _DailyFrenchCashViewState extends State<DailyFrenchCashView> {
               ? widget.pageData.nameEn
               : widget.pageData.nameAr,
         ),
-        body: BlocProvider(
-          create: (context) => DailyFrenchCashCubit(getIt.get<DailyFrenchCashRepoImpl>()),
-          child: const DailyFrenchCashViewBody(),
+        body: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) =>
+                  AccountsCubit(getIt.get<DailyFrenchCashRepoImpl>())..getAccountsList(),
+            ),
+            BlocProvider(
+              create: (context) =>
+                  DailyFrenchCashCubit(getIt.get<DailyFrenchCashRepoImpl>()),
+            ),
+          ],
+          child: BlocBuilder<AccountsCubit, AccountsState>(
+            builder: (context, state) {
+              if (state is AccountsSuccess) {
+                state.accountsList;
+                return DailyFrenchCashViewBody(
+                  accountsList: state.accountsList,
+                );
+              } else if (state is AccountsFailure) {
+                return CustomErrorMassage(errorMassage: state.errorMassage);
+              } else {
+                return const CustomLoadingWidget();
+              }
+            },
+          ),
         ),
       ),
     );
--- lib/features/dailyFrenchCash/presentation/widgets/daily_french_cash_view_body.dart
+++ lib/features/dailyFrenchCash/presentation/widgets/daily_french_cash_view_body.dart
@@ -1,12 +1,49 @@
+import 'package:erp_system/core/widgets/custom_error_massage.dart';
+import 'package:erp_system/core/widgets/custom_loading_widget.dart';
+import 'package:erp_system/features/dailyFrenchCash/data/models/body_request_daily_french_cash.dart';
+import 'package:erp_system/features/dailyFrenchCash/presentation/manager/dailyFrenchCash/daily_french_cash_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DailyFrenchCashViewBody extends StatelessWidget {
-  const DailyFrenchCashViewBody({super.key});
+import '../../data/models/accounts_model.dart';
+import 'build_daily_french_cash_input.dart';
+
+class DailyFrenchCashViewBody extends StatefulWidget {
+  const DailyFrenchCashViewBody({super.key, required this.accountsList});
+  final List<AccountsList> accountsList;
 
   @override
+  State<DailyFrenchCashViewBody> createState() => _DailyFrenchCashViewBodyState();
+}
+
+class _DailyFrenchCashViewBodyState extends State<DailyFrenchCashViewBody> {
+  late BodyRequestDailyFrenchCash inputMyData;
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      children: [
+        BuildDailyFrenchCashInput(
+          accountsList: widget.accountsList,
+          dataInput: (dataInput) {
+            setState(() {
+              inputMyData = dataInput;
+            });
+          },
+        ),
+        BlocBuilder<DailyFrenchCashCubit, DailyFrenchCashState>(
+          builder: (context, state) {
+            if (state is DailyFrenchCashLoading) {
+              return const CustomLoadingWidget();
+            } else if (state is DailyFrenchCashFailure) {
+              return CustomErrorMassage(errorMassage: state.errorMassage);
+            } else if (state is DailyFrenchCashSuccess) {
+              return Text("SUCCESS");
+            } else {
+              return Text("OFF");
+            }
+          },
+        )
+      ],
     );
   }
 }
