ba0097554e43459f6624b462965d67260709b551
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-03-17 16:52:22 +0200

    create custom_table_daily_french_cash.dart

---
+++ lib/features/dailyFrenchCash/presentation/widgets/custom_table_daily_french_cash.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../data/models/daily_french_cash_model.dart';
+
+class CustomTableDailyFrenchCash extends StatelessWidget {
+  const CustomTableDailyFrenchCash({super.key, required this.dailyFrenchCashModel});
+  final DailyFrenchCashModel dailyFrenchCashModel;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/features/dailyFrenchCash/presentation/widgets/daily_french_cash_view_body.dart
+++ lib/features/dailyFrenchCash/presentation/widgets/daily_french_cash_view_body.dart
@@ -2,10 +2,13 @@ import 'package:erp_system/core/widgets/custom_error_massage.dart';
 import 'package:erp_system/core/widgets/custom_loading_widget.dart';
 import 'package:erp_system/features/dailyFrenchCash/data/models/body_request_daily_french_cash.dart';
 import 'package:erp_system/features/dailyFrenchCash/presentation/manager/dailyFrenchCash/daily_french_cash_cubit.dart';
+import 'package:erp_system/features/dailyFrenchCash/presentation/widgets/custom_table_daily_french_cash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../../data/models/accounts_model.dart';
+import '../../data/models/daily_french_cash_model.dart';
 import 'build_daily_french_cash_input.dart';
 
 class DailyFrenchCashViewBody extends StatefulWidget {
@@ -20,30 +23,54 @@ class _DailyFrenchCashViewBodyState extends State<DailyFrenchCashViewBody> {
   late BodyRequestDailyFrenchCash inputMyData;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        BuildDailyFrenchCashInput(
-          accountsList: widget.accountsList,
-          dataInput: (dataInput) {
-            setState(() {
-              inputMyData = dataInput;
-            });
-          },
-        ),
-        BlocBuilder<DailyFrenchCashCubit, DailyFrenchCashState>(
-          builder: (context, state) {
-            if (state is DailyFrenchCashLoading) {
-              return const CustomLoadingWidget();
-            } else if (state is DailyFrenchCashFailure) {
-              return CustomErrorMassage(errorMassage: state.errorMassage);
-            } else if (state is DailyFrenchCashSuccess) {
-              return Text("SUCCESS");
-            } else {
-              return Text("OFF");
-            }
-          },
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+      child: Column(
+        children: [
+          Card(
+            color: Colors.white,
+            child: BuildDailyFrenchCashInput(
+              accountsList: widget.accountsList,
+              dataInput: (dataInput) {
+                setState(() {
+                  inputMyData = dataInput;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 8),
+          BlocBuilder<DailyFrenchCashCubit, DailyFrenchCashState>(
+            builder: (context, state) {
+              if (state is DailyFrenchCashLoading) {
+                return const CustomLoadingWidget();
+              } else if (state is DailyFrenchCashFailure) {
+                return CustomErrorMassage(errorMassage: state.errorMassage);
+              } else if (state is DailyFrenchCashSuccess) {
+                DailyFrenchCashModel dailyFrenchCashModel = state.dailyFrenchCashModel;
+                String title = inputMyData.creditOrDepit
+                    ? "واردة من حساب من فترة ${DateFormat('MMM d, y', 'en').format(DateTime.parse(inputMyData.dateFrom))} إلى فترة ${DateFormat('MMM d, y', 'en').format(DateTime.parse(inputMyData.dateTo))}"
+                    : "صادرة من حساب من فترة ${DateFormat('MMM d, y', 'en').format(DateTime.parse(inputMyData.dateFrom))} إلى فترة ${DateFormat('MMM d, y', 'en').format(DateTime.parse(inputMyData.dateTo))}";
+                return Card(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(title),
+                      ),
+                      CustomTableDailyFrenchCash(
+                        dailyFrenchCashModel: dailyFrenchCashModel,
+                      )
+                    ],
+                  ),
+                );
+              } else {
+                return const SizedBox();
+              }
+            },
+          )
+        ],
+      ),
     );
   }
 }
