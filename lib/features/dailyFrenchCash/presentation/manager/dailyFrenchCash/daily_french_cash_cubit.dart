9f0d61bd4db675ef94262706e97013dbc690ea2c
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-03-17 11:48:11 +0200

    create daily_french_cash_cubit.dart

--- lib/features/dailyFrenchCash/presentation/manager/dailyFrenchCash/daily_french_cash_cubit.dart
+++ lib/features/dailyFrenchCash/presentation/manager/dailyFrenchCash/daily_french_cash_cubit.dart
@@ -1,9 +1,26 @@
-import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:erp_system/features/dailyFrenchCash/data/models/daily_french_cash_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/errors/failures.dart';
+import '../../../data/models/body_request_daily_french_cash.dart';
+import '../../../data/models/daily_french_cash_model.dart';
+import '../../../data/repositories/daily_french_cash_repo.dart';
 
 part 'daily_french_cash_state.dart';
 
 class DailyFrenchCashCubit extends Cubit<DailyFrenchCashState> {
-  DailyFrenchCashCubit() : super(DailyFrenchCashInitial());
+  DailyFrenchCashCubit(this.dailyFrenchCashRepo) : super(DailyFrenchCashInitial());
+  final DailyFrenchCashRepo dailyFrenchCashRepo;
+  Future<void> getDailyFrenchCash(
+      {required BodyRequestDailyFrenchCash bodyRequestDailyFrenchCash}) async {
+    emit(DailyFrenchCashLoading());
+    Either<Failure, DailyFrenchCashModel> result = await dailyFrenchCashRepo
+        .getDailyFrenchCash(bodyRequestDailyFrenchCash: bodyRequestDailyFrenchCash);
+    result.fold((failure) {
+      emit(DailyFrenchCashFailure(failure.errorMassage));
+    }, (data) {
+      emit(DailyFrenchCashSuccess(data));
+    });
+  }
 }
