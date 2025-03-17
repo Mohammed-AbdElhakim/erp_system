6cbd16c60c5b7bb66cd4c9ee5c14d066a89ba825
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-03-17 11:26:13 +0200

    create daily_french_cash_repo_impl.dart

---
+++ lib/features/dailyFrenchCash/data/repositories/daily_french_cash_repo_impl.dart
@@ -0,0 +1,48 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/helper/SharedPreferences/pref.dart';
+import '../../../../core/utils/api_service.dart';
+import '../../../../core/utils/app_strings.dart';
+import '../models/body_request_daily_french_cash.dart';
+import '../models/daily_french_cash_model.dart';
+import 'daily_french_cash_repo.dart';
+
+class DailyFrenchCashRepoImpl implements DailyFrenchCashRepo {
+  final ApiService apiService;
+
+  DailyFrenchCashRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, DailyFrenchCashModel>> getDailyFrenchCash(
+      {required BodyRequestDailyFrenchCash bodyRequestDailyFrenchCash}) async {
+    try {
+      String companyKey =
+          await Pref.getStringFromPref(key: AppStrings.companyIdentifierKey) ?? "";
+      String token = await Pref.getStringFromPref(key: AppStrings.tokenKey) ?? "";
+      // var rrr = jsonEncode(bodyRequestDailyFrenchCash.toJson());
+      Map<String, dynamic> data = await apiService.post(
+        endPoint: "web/DailyFrenshCash/PostFrensh",
+        data: bodyRequestDailyFrenchCash,
+        headers: {
+          "Authorization": "Bearer $token",
+          "CompanyKey": companyKey,
+        },
+      );
+      DailyFrenchCashModel dailyFrenchCashModel = DailyFrenchCashModel.fromJson(data);
+
+      return right(dailyFrenchCashModel);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+}
