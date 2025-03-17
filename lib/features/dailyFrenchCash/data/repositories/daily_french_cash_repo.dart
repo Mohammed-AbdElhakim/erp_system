3297b4840a04ce674eb13ad210382e7e468bae15
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-03-17 12:52:13 +0200

    add function in repo to get accounts list

--- lib/features/dailyFrenchCash/data/repositories/daily_french_cash_repo.dart
+++ lib/features/dailyFrenchCash/data/repositories/daily_french_cash_repo.dart
@@ -1,10 +1,13 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failures.dart';
+import '../models/accounts_model.dart';
 import '../models/body_request_daily_french_cash.dart';
 import '../models/daily_french_cash_model.dart';
 
 abstract class DailyFrenchCashRepo {
   Future<Either<Failure, DailyFrenchCashModel>> getDailyFrenchCash(
       {required BodyRequestDailyFrenchCash bodyRequestDailyFrenchCash});
+
+  Future<Either<Failure, AccountsModel>> getAccountsList();
 }
--- lib/features/dailyFrenchCash/data/repositories/daily_french_cash_repo_impl.dart
+++ lib/features/dailyFrenchCash/data/repositories/daily_french_cash_repo_impl.dart
@@ -5,6 +5,7 @@ import '../../../../core/errors/failures.dart';
 import '../../../../core/helper/SharedPreferences/pref.dart';
 import '../../../../core/utils/api_service.dart';
 import '../../../../core/utils/app_strings.dart';
+import '../models/accounts_model.dart';
 import '../models/body_request_daily_french_cash.dart';
 import '../models/daily_french_cash_model.dart';
 import 'daily_french_cash_repo.dart';
@@ -32,6 +33,37 @@ class DailyFrenchCashRepoImpl implements DailyFrenchCashRepo {
       DailyFrenchCashModel dailyFrenchCashModel = DailyFrenchCashModel.fromJson(data);
 
       return right(dailyFrenchCashModel);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, AccountsModel>> getAccountsList() async {
+    try {
+      String companyKey =
+          await Pref.getStringFromPref(key: AppStrings.companyIdentifierKey) ?? "";
+      String token = await Pref.getStringFromPref(key: AppStrings.tokenKey) ?? "";
+      Map<String, dynamic> data = await apiService.post(
+        endPoint: "web/Structure/getDataGlobal",
+        data: {"tableName": "MonyAccount"},
+        headers: {
+          "Authorization": "Bearer $token",
+          "CompanyKey": companyKey,
+        },
+      );
+      AccountsModel accountsModel = AccountsModel.fromJson(data);
+
+      return right(accountsModel);
     } catch (e) {
       if (e is DioException) {
         return left(
