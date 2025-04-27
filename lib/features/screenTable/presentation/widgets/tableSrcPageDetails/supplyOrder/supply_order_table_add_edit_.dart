152f33425c8c86d5b5c67aa89225e013d167146e
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2025-04-27 15:51:45 +0300

    refactor code

--- lib/features/screenTable/presentation/widgets/tableSrcPageDetails/supplyOrder/supply_order_table_add_edit_.dart
+++ lib/features/screenTable/presentation/widgets/tableSrcPageDetails/supplyOrder/supply_order_table_add_edit_.dart
@@ -1,4 +1,3 @@
-import 'package:erp_system/features/screenTable/presentation/widgets/tableSrcPageDetails/purchases/add_purchases.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:linked_scroll_controller/linked_scroll_controller.dart';
